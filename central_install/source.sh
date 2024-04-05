b4dd057caa935e685b02da8cbb152a80a02075a1
lucaparisi91 <luca.parisi91@gamil.com>
2024-04-05 18:34:03 +0100

    Updated configuration for a central and user spack installation. Documentation not yet added.

---
+++ central_install/source.sh
@@ -0,0 +1,3 @@
+export SPACK_USER_CACHE_PATH=${SPACK_ROOT_EPCC}/.spack
+
+source $SPACK_ROOT/share/spack/setup-env.sh
---
+++ user_install/source.sh
@@ -0,0 +1,3 @@
+export SPACK_USER_CACHE_PATH=/work/$(id -gn)/$(id -gn)/$(id -un)/.spack
+
+source $SPACK_ROOT/share/spack/setup-env.sh
