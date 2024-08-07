65ae6a5497209d5fa11e8f9193e51c99000553a9
nmannall <n.mannall@epcc.ed.ac.uk>
2024-08-07 17:00:18 +0100

    Remove boilerplate comment

--- var/spack/repos/archer2/packages/castep/package.py
+++ var/spack/repos/archer2/packages/castep/package.py
@@ -2,23 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install castep
-#
-# You can edit this file again by typing:
-#
-#     spack edit castep
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 import os
 
 from spack.package import *
